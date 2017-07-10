2cf0b697d66743198bd3c711f73b48d20d839792
HannaMyr <Ania110995@mail.ru>
2017-07-10 21:38:57 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Prognoz pogod</title>
+<link rel="stylesheet" href="css/main.css" type="text/css" charset="utf-8">
+<meta name="decription" content="Prognoz pogod,prognoz,pogod">
+<meta name="keywords" content="Prognoz pogod,prognoz,pogod">
+<meta name="viewport" content="Prognoz pogod,prognoz,pogod">
+<link href="img/.ico" rel="shortcut icon" type="image/x-icon">
+</head>
+<body>
+<div id="wrapper">
+<div id="content">
+<header>
+<div id="logo">
+<a href="https://itproger.com/" title="Na glavnyu">
+<img src="img/logo.png" title="ItProger.com" alt="itProger.com">
+<span>Progboz pogod</span>
+</a>
+</div>
+<div id="about">
+<a href="" title="Yznat detalno o recklame">Reclama</a>
+<a href="" title="Napisat nam sms">obratna svyz</a>
+</div>
+<div id="reg_auth">
+<a href="" title="Vvoyti v kabinet">
+<div id="btn">
+Registraca
+</div>
+</a>
+</div>
+</header>
+</div>
+<footer>
+<div id="site_name">
+<span>Prognoz</span> - pogoda dly tebya
+</div>
+<div id="clear"></div>
+<div id="footer_menu">
+<a href="" title="Yznat pro reclamy">Reclama</a>
+<a href="" title="Podderzat">Podderzhat proekt</a>
+<a href="" title="Napisat pismo">Obratna svyz</a>
+</div>
+<div id="rights">
+<a href="">Vse prava zachi4eni &copy; <?=date ('Y')?></a>
+</div>
+</footer>
+</div>
+</body>
+
+</html>
